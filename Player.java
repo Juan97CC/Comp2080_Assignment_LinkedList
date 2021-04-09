8a944a64618044fbe31047110a1d45f421a173f6
Juan97CC <59619230+Juan97CC@users.noreply.github.com>
2021-04-09 15:09:07 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,84 @@
+public class Player
+{
+    public String name;
+    public Backpack backpack;
+    public int numItems;
+    public double money;
+    public double currWeight;
+    public double maxWeight;
+    public int maxItems;
+
+    public Player(String n, double m)
+    {
+        name = n;
+        money = m;
+        numItems = 0;
+        backpack = new Backpack();
+        currWeight = 0;
+        maxWeight = 90;
+        maxItems = 31;
+    }
+
+    public void buy(Weapon w)
+    {
+        if ((w.weight + currWeight) <= maxWeight && money >= w.cost && numItems < maxItems )  {
+            Backpack.addWeapon(w); // add to list
+            withdraw(w.cost); // remove weapon cost from total
+            System.out.println(w.weaponName + " bought...");
+            numItems++;
+            currWeight += w.weight;
+           // System.out.println(numItems);
+           // System.out.println(currWeight);
+
+        }
+
+        else System.out.println("Sorry :( transaction could not be completed \n");
+    }
+
+
+    public void withdraw(double amt)
+    {
+
+            money = money - amt;
+
+    }
+
+    public boolean inventoryFull()
+    {
+        return (numItems == 30) ;
+    }
+
+    public void printCharacter()
+    {
+        System.out.println("Name: "+name+
+                "\nMoney: "+money +
+                "\nBack pack weight: " +currWeight+
+                "\nCurrent Items(In Backpack): " + numItems);
+
+    }
+
+    public void printBackpack()
+    {
+        System.out.println(
+                name +", you own "+numItems+" of Weapons," +
+                "total weight is " + currWeight);
+        backpack.displayTable();
+
+    }
+
+    public void removeBackpackItem(String weapon, int damage, int range)
+    {
+        Weapon v = backpack.search(weapon, damage, range);
+        if (v != null){
+            backpack.delete(v);
+            currWeight -= v.weight;
+            numItems--;
+            System.out.println("Item has been deleted");
+        }
+        else System.out.println("Sorry :( item doesn't exist\n\n");
+
+    }
+
+
+
+}
